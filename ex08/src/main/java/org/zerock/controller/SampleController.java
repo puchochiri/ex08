ef68f06acc4bf0e6f72de7c1d358e9aaa62e62ab
양선우 <@DESKTOP-94O1RKP>
2022-10-17 12:10:13 +0900

    first commit

---
+++ ex08/src/main/java/org/zerock/controller/SampleController.java
@@ -0,0 +1,51 @@
+package org.zerock.controller;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.zerock.domain.SampleDTO;
+import org.zerock.domain.SampleDTOList;
+import org.zerock.domain.TodoDTO;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@RequestMapping("/sample/*")
+@Log4j
+public class SampleController {
+	
+	@GetMapping("/all")
+	public void doAll() {
+		
+		log.info("do all can access everybody");
+	}
+	
+	@GetMapping("/member")
+	public void doMember() {
+		
+		log.info("logined member");
+		
+	}
+	
+	@GetMapping("/admin")
+	public void doAdmin() {
+		
+		log.info("admin only");
+	}
+	
+	
+
+
+}
